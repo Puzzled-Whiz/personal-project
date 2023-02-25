108a78182e13ff43b50540dec78baa5346b680ab
Puzzled-Whiz <migueltroncoso71@gmail.com>
2023-02-24 21:37:30 -0500

    footer addded & other small edits. filled in homepage site details

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,10 @@
+// Image slider/carousel
 const buttons = document.querySelectorAll("[data-carousel-button]");
 
 buttons.forEach((button) => {
   button.addEventListener("click", () => {
     const offset = button.dataset.carouselButton === "next" ? 1 : -1;
-    const slides = button
-      .closest("[data-carousel]")
-      .querySelector("[data-slides]");
+    const slides = button.closest("[data-carousel]").querySelector("[data-slides]");
 
     const activeSlide = slides.querySelector("[data-active]");
     let newIndex = [...slides.children].indexOf(activeSlide) + offset;
